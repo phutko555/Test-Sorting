23748b9a16db84ff10d1b5cc2544c44d3e4b948f
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-30 22:21:12 +0000

    Update Sorting.java

--- src/main/java/com/epam/rd/autotasks/Sorting.java
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -1,7 +1,11 @@
 package com.epam.rd.autotasks;
 
 public class Sorting {
-    public void sort(int[] array){
-        throw new UnsupportedOperationException();
+
+    public void sort(int[] array) {
+        if (array == null) {
+            throw new IllegalArgumentException();
+        }
+        java.util.Arrays.sort(array);
     }
 }
