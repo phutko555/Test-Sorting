6aeadab7478c100abb21ed4c9218c5d1a2821109
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-01-30 22:22:18 +0000

    Update SortingTest.java

--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -1,5 +1,5 @@
 package com.epam.rd.autotasks;
-
+import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
 
 
@@ -7,30 +7,40 @@ public class SortingTest {
 
     Sorting sorting = new Sorting();
 
-    @Test
-    public void testNullCase(){
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullCase() {
+        sorting.sort(null);
     }
 
     @Test
-    public void testEmptyCase(){
-
+    public void testEmptyCase() {
+        int[] array = {};
+        sorting.sort(array);
+        assertArrayEquals(new int[]{}, array);
     }
 
     @Test
     public void testSingleElementArrayCase() {
-
+        int[] array = {5};
+        sorting.sort(array);
+        assertArrayEquals(new int[]{5}, array);
     }
 
     @Test
     public void testSortedArraysCase() {
-
+        int[] array = {1, 2, 3, 4, 5};
+        sorting.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
     }
 
     @Test
     public void testOtherCases() {
-
+        int[] array = {5, 3, 1, 4, 2};
+        sorting.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
     }
-
 }
+
+
+
 
